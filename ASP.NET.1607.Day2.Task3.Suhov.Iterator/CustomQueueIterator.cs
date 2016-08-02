223a48cdf97f09a8f53f866d748b63f43e72ceaa
vladimir_suhov <vladimir_suhov@outlook.com>
2016-08-02 09:08:48 -0700

    initial

--- ASP.NET.1607.Day2.Task3.Suhov.Iterator/CustomQueueIterator.cs
+++ ASP.NET.1607.Day2.Task3.Suhov.Iterator/CustomQueueIterator.cs
@@ -8,13 +8,25 @@ namespace ASP.NET._1607.Day2.Task3.Suhov.Iterator
 {
     public class CustomQueueIterator<T>
     {
+        #region Private Fields
         private readonly CustomQueue<T> _collection;
         private int _currentIndex;
+        #endregion
+        /// <summary>
+        /// Default constructor without parameters
+        /// </summary
+        public CustomQueueIterator(){ }
+        /// <summary>
+        /// Constructor with parameter
+        /// </summary
         public CustomQueueIterator(CustomQueue<T> collection)
         {
             _currentIndex = -1;
             _collection = GetCollection(collection);
         }
+        /// <summary>
+        /// Returns the current item from collection
+        /// </summary
         public T Current
         {
             get
@@ -24,10 +36,16 @@ namespace ASP.NET._1607.Day2.Task3.Suhov.Iterator
                 return _collection[_currentIndex];
             }
         }
+        /// <summary>
+        /// Set the index to default value
+        /// </summary
         public void Reset()
         {
             _currentIndex = -1;
         }
+        /// <summary>
+        /// Return true if there are more items to iterate
+        /// </summary
         public bool MoveNext()
         {
             return ++_currentIndex < _collection.Count;
